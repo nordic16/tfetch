fc79d535f59ee84883d9255317976ceadcedc5c9
xkyfal <diogo6204@gmail.com>
2025-01-26 13:06:06 +0000

    cleanup

--- src/cli/tfetch.rs
+++ src/cli/tfetch.rs
@@ -1,23 +1,19 @@
-use std::{io::{self, Write}, sync::mpsc::channel};
-
+use std::{io::Write, time::Duration};
 use console::{Style, Term};
-use reqwest::{Client, ClientBuilder};
-
-use crate::{cli::TMessage, providers::{self, braflix::Braflix, Provider}};
+use indicatif::ProgressBar;
+use reqwest::Client;
+use crate::providers::{braflix::Braflix, Provider};
 
 fn stdout() -> Term {
     Term::stdout()
 }
 
-
 pub async fn run() -> anyhow::Result<()> {
     let client : Client = Client::builder().referer(true).build()?;
 
-    print!("{}", "\x1B[2J\x1B[1;1H"); // clears term and sets cursor pos to (1,1)
     let term = stdout();
+    term.clear_screen()?;
     let title = format!("{}", get_style().apply_to("Welcome to tfetch!\n"));
-
-
 
     term.hide_cursor()?;
 
@@ -25,8 +21,14 @@ pub async fn run() -> anyhow::Result<()> {
     write!(&term, "Search TV shows or anime: ")?; // I did NOT need that newline.
     
     let query = term.read_line()?;
+    term.clear_screen()?;
+
+    let bar = ProgressBar::new_spinner().with_message("Loading results");
+    bar.enable_steady_tick(Duration::from_millis(100));
+
     let results = Braflix::fetch_results(&client, &query).await?;
     
+    bar.finish();
     Ok(())
 }
 
--- src/tests/braflix.rs
+++ src/tests/braflix.rs
@@ -1,15 +1,15 @@
-use reqwest::Client;
-
-use crate::providers::{braflix::Braflix, Provider};
 
 
 #[tokio::test]
 async fn fetch_results() -> anyhow::Result<()> {
+    use reqwest::Client;
+    use crate::providers::{braflix::Braflix, Provider};
+
     let client = Client::new();
     let query = "one piece";
     let res = Braflix::fetch_results(&client, &query).await?;
 
     res.iter().for_each(|f| println!("{}", f));
-    Ok(())
 
+    Ok(())
 }
